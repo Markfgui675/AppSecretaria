df4a9e991f73dd71e6ccbe5e42c437a0295fb9cb
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-04-04 18:00:50 -0300

    Feature:
    Nova navigation_drawer.

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,8 +3,9 @@ import 'package:app_secretaria_flutter/telas/ita.dart';
 import 'package:app_secretaria_flutter/telas/quem.dart';
 import 'package:app_secretaria_flutter/telas/quizes.dart';
 import 'package:app_secretaria_flutter/telas/inicio.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:app_secretaria_flutter/widgets/header_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,54 +16,133 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  int _indice = 2;
+  var currentPage = DrawerSections.inicio;
+  dynamic atualPage = 'Início';
 
   @override
   Widget build(BuildContext context) {
-
-    List<Widget> telas = [
-      Quizes(),
-      const Quem(),
-      const Inicio(),
-      const Ita(),
-      const Configuracoes()
-    ];
-
+    var container;
+    if (currentPage == DrawerSections.inicio) {
+      container = Inicio();
+      atualPage = 'Início';
+    } else if (currentPage == DrawerSections.quem) {
+      container = Quem();
+      atualPage = 'Quem é quem';
+    } else if (currentPage == DrawerSections.ita) {
+      container = Ita();
+      atualPage = 'Ita';
+    } else if (currentPage == DrawerSections.quizes) {
+      container = Quizes();
+      atualPage = 'Quizes';
+    } else if (currentPage == DrawerSections.configuracoes) {
+      container = Configuracoes();
+      atualPage = 'Configurações';
+    }
     return Scaffold(
-
-      backgroundColor: Colors.white,
-
-
-      body: Center(
-        child: Container(
-            padding: const EdgeInsets.only(top: 50),
-            child: telas[_indice]
+      appBar: AppBar(
+        backgroundColor: Color(0xff2E6EA7),
+        elevation: 0,
+        centerTitle: true,
+        title: Text(atualPage,
+          style: GoogleFonts.kanit().copyWith(fontWeight: FontWeight.bold, fontSize: 22, color: Colors.white),
+      )),
+      body: container,
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(70))
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                HeaderDrawer(),
+                MyDrawerList(),
+              ],
+            ),
+          ),
         ),
       ),
-
-
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 65.0,
-        backgroundColor: Colors.white,
-        buttonBackgroundColor: const Color(0xff2E6EA7),
-        color: const Color(0xff2E6EA7),
-        animationDuration: const Duration(milliseconds: 300),
-        index: _indice,
-        onTap: (indice){
-          setState(() {
-            _indice = indice;
-          });
-        },
-        items: [
-          Image.asset('icons/quiz.png', width: 32),
-          Image.asset('icons/customer.png', width: 32),
-          Image.asset('icons/home.png', width: 32),
-          Image.asset('icons/ita_icone.png', width: 32),
-          Icon(Icons.account_circle, size: 34, color: Colors.white,)
-        ],
-
-      ),
-
     );
   }
+
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        // shows the list of menu drawer
+        children: [
+          menuItem(1, "Início", 'icons/home.png',
+              currentPage == DrawerSections.inicio ? true : false, false),
+          menuItem(2, "Quem é quem", 'icons/customer.png',
+              currentPage == DrawerSections.quem ? true : false, false),
+          menuItem(3, "Ita", 'icons/ita_icone.png',
+              currentPage == DrawerSections.ita ? true : false, true),
+          menuItem(4, "Quizes", 'icons/quiz.png',
+              currentPage == DrawerSections.quizes ? true : false, false),
+          SizedBox(height: 330),
+          menuItem(5, "Configurações", 'icons/setting.png',
+              currentPage == DrawerSections.configuracoes ? true : false, false),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(int id, String title, String urlIcon, bool selected, bool ita) {
+    return Padding(
+        padding: const EdgeInsets.only(right: 12, left: 12),
+      child: Material(
+        color: selected ? Colors.grey[200] : Colors.transparent,
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+            setState(() {
+              if (id == 1) {
+                currentPage = DrawerSections.inicio;
+              } else if (id == 2) {
+                currentPage = DrawerSections.quem;
+              } else if (id == 3) {
+                currentPage = DrawerSections.ita;
+              } else if (id == 4) {
+                currentPage = DrawerSections.quizes;
+              } else if (id == 5) {
+                currentPage = DrawerSections.configuracoes;
+              }
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: ita ?
+                      Image.asset(urlIcon, width: 30, height: 30,) :
+                      Image.asset(urlIcon, width: 30, height: 30, color: Colors.black,)
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    title,
+                    style: GoogleFonts.kanit().copyWith(fontWeight: FontWeight.bold, fontSize: 18, color: const Color(0xff2E6EA7)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+enum DrawerSections {
+  inicio,
+  quem,
+  ita,
+  quizes,
+  configuracoes
 }
---
+++ lib/widgets/header_drawer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeaderDrawer extends StatefulWidget {
+  const HeaderDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<HeaderDrawer> createState() => _HeaderDrawerState();
+}
+
+class _HeaderDrawerState extends State<HeaderDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 50, right: 12, left: 12),
+      color: Colors.white,
+      width: double.infinity,
+      height: 150,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('NomeUsario',
+                style: GoogleFonts.kanit().copyWith(fontWeight: FontWeight.bold, fontSize: 26, color: Colors.black),
+              ),
+              Text('Email@usario',
+                style: GoogleFonts.kanit().copyWith(fontWeight: FontWeight.normal, fontSize: 14, color: Colors.grey[900]),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('imagens/secretaria_teste.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
