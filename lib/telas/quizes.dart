cd975e34e5b0ca528bbcd8254d4288ea29aa617d
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-04-06 20:12:02 -0300

    Chore:
    refatoração da pasta quiz

--- lib/telas/quizes.dart
+++ lib/telas/quizes.dart
@@ -1,8 +1,5 @@
 import 'dart:ui';
 
-import 'package:app_secretaria_flutter/quiz/criar_quiz.dart';
-import 'package:app_secretaria_flutter/quiz/responder_quiz.dart';
-import 'package:app_secretaria_flutter/quiz/titulo_quiz.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
