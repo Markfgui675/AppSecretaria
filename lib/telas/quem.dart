d99f12b4d8da44585b3743106995906c9db88b67
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-04-10 23:37:16 -0300

    Feature:
    Adição de filtros, botão de pesquisa e de saiba mais.

--- lib/telas/quem.dart
+++ lib/telas/quem.dart
@@ -14,16 +14,71 @@ class _QuemState extends State<Quem> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: SingleChildScrollView(
         child: Container(
+          width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text('Quem')
+            children: [
+
+              //Filtros e botão de pesquisar
+              Container(
+                width: double.infinity,
+                height: 90,
+                color: Colors.red,
+                padding: EdgeInsets.all(12),
+                child:
+                    ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          child: Text('Filtro', style: TextStyle(fontSize: 48),),
+                        ),
+                        Container(
+                          child: Text('Filtro', style: TextStyle(fontSize: 48),),
+                        ),
+                        Container(
+                          child: Text('Filtro', style: TextStyle(fontSize: 48),),
+                        ),
+                        Container(
+                          child: Text('Filtro', style: TextStyle(fontSize: 48),),
+                        ),
+                      ],
+                )
+              ),
+              Container(
+                width: double.infinity,
+                color: Colors.yellow,
+                padding: EdgeInsets.only(left: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                        onPressed: (){},
+                        child: Text('Pesquisar')
+                    )
+                  ],
+                ),
+              )
+
+
+
+
+
             ],
           ),
         ),
+      ),
+
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: (){},
+          label: Text(
+            'Saiba mais',
+            style: GoogleFonts.kanit().copyWith(fontWeight: FontWeight.bold, fontSize: 18, color: Color(0xff2E6EA7)),
+          ),
+          elevation: 5,
+          backgroundColor: const Color(0xfff2ab11),
       ),
     );
   }
